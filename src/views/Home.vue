85b1a6726745b5d04f72e8352ca799035c1798d3
Klara <Klara@LAPTOP-Q1PG1K74>
2019-04-13 18:02:38 +0200

    refactoring

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,7 +20,6 @@ import OrsDirections from 'openrouteservice-js/src/OrsDirections';
 import axios from 'axios';
 import { LMap, LTileLayer, LMarker, LPopup, LTooltip, LImageOverlay, LPolyline} from 'vue2-leaflet';
 import 'leaflet/dist/leaflet.css'
-//import * as polyutil from 'polyline-encoded';
 
 
 @Component({
@@ -36,128 +35,97 @@ import 'leaflet/dist/leaflet.css'
 })
 
 export default class Home extends Vue {
-  startpoint='';
-  endpoint='';
-  startpos='';
-  currentPosLong: number|undefined;
-  currentPosLat: number|undefined;
-  latlngPos='';
-  polylinearray: []|undefined;
-  public pos:any;
-  //map='';
-  //marker='';
-  lat: number|undefined;
-  lng: number|undefined;
+  startpoint = '';
+  endpoint = '';
+  startpos = '';
+  currentPosLong: number | undefined;
+  currentPosLat: number | undefined;
+  polylinearray: [] | undefined;
+  steps: [] | undefined;
+  lat: number | undefined;
+  lng: number | undefined;
+
   planroute() {
-    this.getDircetions(this.latlngPos, this.endpoint);
-    /* var map = this.$refs.map.mapObject;
-    var polyline = L.polyline(this.polylinearray,{
-      color:'blue',
-      weight: 6,
-      opacity: 0.9
-    }).addTo(map);
-    map.fitBounds(polyline.getBounds()); */
+    this.getDircetions(this.endpoint);
   }
   
-  insertPolyline(){
-    /*var map = this.$refs.map.mapObject;
-    L.polyline(this.polylinearray,{
-      color:'blue'
-    }).addTo(map);*/
-  }
-
-  async getReverseGeocode(){
-    await axios.get('https://api.openrouteservice.org/geocode/reverse?',{
-      params:{
-        'point.lat': this.currentPosLat,
-        'point.lon': this.currentPosLong,
-        api_key: '5b3ce3597851110001cf6248284f5c01dda445a9a0406fe843f2ccb5',
-      }
-    })
-    .then((response) => {
-      console.log(response);
-      this.startpos=response.data.features[0].properties.name + ',' + response.data.features[0].properties.postalcode;
-      console.log(this.startpos);
-    })
-    .catch(function (error:any) {
-      console.log(error);
-    });
+  async getReverseGeocode() {
+    await axios.get('https://api.openrouteservice.org/geocode/reverse?', {
+        params: {
+          'point.lat': this.currentPosLat,
+          'point.lon': this.currentPosLong,
+          api_key: '5b3ce3597851110001cf6248284f5c01dda445a9a0406fe843f2ccb5',
+        }
+      })
+      .then((response) => {
+        this.startpos = response.data.features[0].properties.name + ',' + response.data.features[0].properties.postalcode;
+      })
+      .catch(function (error: any) {
+        console.log(error);
+      });
     return this.startpos;
   }
 
 
-  getPosition(){
+  async getPosition() {
+    this.getCurrentPosition();
+    this.startpoint = await this.getReverseGeocode();
+  }
+
+
+  getCurrentPosition() {
     navigator.geolocation.getCurrentPosition(
-      async (position)=>{
-        this.currentPosLong = position.coords.longitude;
-        this.currentPosLat = position.coords.latitude;
-        this.latlngPos = this.currentPosLat + ','+ this.currentPosLong;
-        console.log(this.currentPosLat);
-        console.log(this.currentPosLong); 
-        this.startpoint = await this.getReverseGeocode();
-        //this.$refs.map.mapObject.setView(this.latlngPos, 13);
-      },
-      function(error){
-        alert(error.message);
-      }
+      async (position) => {
+          this.currentPosLong = position.coords.longitude;
+          this.currentPosLat = position.coords.latitude;
+        },
+        function (error) {
+          alert(error.message);
+        }
     );
   }
 
- data() {
+  data() {
     return {
       zoom: 13,
-      center: [48,9],
+      center: [48, 9],
       url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-      polyline:{
-        //latlngs: await this.getDircetions(this.startpoint,this.endpoint),
-        //color: 'green'
-      }
-      //attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      //marker: L.latLng(47.413220, -1.219482),
-      //markers: []
     }
   }
-  mounted () {
-    //this.$refs.map.mapObject.setView(navigator.geolocation.getCurrentPosition((position)=>{this.pos = position}), 13);
+
+  async getgeocode(address: string) {
+    await axios.get('https://api.openrouteservice.org/geocode/search?', {
+        params: {
+          text: address,
+          api_key: '5b3ce3597851110001cf6248284f5c01dda445a9a0406fe843f2ccb5',
+        }
+      })
+      .then((response) => {
+        console.log(response);
+        this.lat = response.data.features[0].geometry.coordinates[1];
+        this.lng = response.data.features[0].geometry.coordinates[0];
+        console.log(this.lat);
+        console.log(this.lng);
+      })
+      .catch(function (error: any) {
+        console.log(error);
+      });
+    return this.lng + ',' + this.lat;
   }
 
-  //Vue.config.productionTip = false;
-
-
-async getgeocode(address: string) {
-  await axios.get('https://api.openrouteservice.org/geocode/search?', {
-      params: {
-        text: address,
-        api_key: '5b3ce3597851110001cf6248284f5c01dda445a9a0406fe843f2ccb5',
-      }
-    })
-    .then((response) => {
-      console.log(response);
-      this.lat = response.data.features[0].geometry.coordinates[1];
-      this.lng = response.data.features[0].geometry.coordinates[0];
-      console.log(this.lat);
-      console.log(this.lng);
-    })
-    .catch(function (error:any) {
-      console.log(error);
-    });
-    return this.lng +','+ this.lat;
-  }
-
-async getDircetions(startaddress: string, endaddress: string){
-  await axios.get('https://api.openrouteservice.org/v2/directions/cycling-regular?',{
-    params:{
-      api_key:  '5b3ce3597851110001cf6248284f5c01dda445a9a0406fe843f2ccb5',
-      start: this.currentPosLong + ',' + this.currentPosLat,
-      end: await this.getgeocode(endaddress),
-    }
-  })
-  .then((response)=>{
-    console.log(response);
-    this.polylinearray = response.data.features[0].geometry.coordinates;
-    console.log(this.polylinearray);
-  })
-  return this.polylinearray;
+  async getDircetions(endaddress: string) {
+    await axios.get('https://api.openrouteservice.org/v2/directions/cycling-regular?', {
+        params: {
+          api_key: '5b3ce3597851110001cf6248284f5c01dda445a9a0406fe843f2ccb5',
+          start: this.currentPosLong + ',' + this.currentPosLat,
+          end: await this.getgeocode(endaddress),
+        }
+      })
+      .then((response) => {
+        console.log(response);
+        this.polylinearray = response.data.features[0].geometry.coordinates;
+        this.steps = response.data.features[0].properties.segments[0].steps;
+      })
   }
 }
 </script>
